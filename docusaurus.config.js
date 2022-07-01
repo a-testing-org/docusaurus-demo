961e533c5316c53b30cc03955871a1248e8f1861
Kimberly Chang <kchang@oleaedge.com>
2022-07-01 08:50:26 -0500

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://kchang12345.github.io/docusaurus-demo',
-  baseUrl: '/',
+  baseUrl: '/docusaurus-demo/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
